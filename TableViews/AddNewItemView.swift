1714f65e99ff93bc97cf3b2b941aca2b2ca0b8bb
Hsuan <hsuan.dev@gmail.com>
2020-04-17 09:37:40 +0800

    ok

---
+++ TableViews/AddNewItemView.swift
@@ -0,0 +1,30 @@
+//
+//  AddNewItemView.swift
+//  TableViews
+//
+//  Created by student on 2020/4/17.
+//  Copyright © 2020 hsuan. All rights reserved.
+//
+
+import UIKit
+
+class AddNewItemView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
